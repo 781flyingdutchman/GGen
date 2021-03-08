3d14c92dea38b3246275d4eacae153e2f3915ebc
Bram Bout <bram@bbflight.com>
2021-03-07 17:09:10 -0800

    Code gen for multi layout appears to work

--- test/multi_workpiece_test.dart
+++ test/multi_workpiece_test.dart
@@ -8,85 +8,114 @@ final testCodeTall =  'G1 X10 Y20 F100';
 
 void main() {
 
-  test('add', () {
-    var w = WorkSimulator(testCode);
-    w.simulate();
-    expect(w.physicalBox, equals(Rect(Point.zero(), Point(10, 10))));
-    var multi = MultiWorkpiece();
-    expect(() => multi.add(WorkpiecePlacement(w, Placement.up)), throwsStateError);
-    multi.add(WorkpiecePlacement(w, Placement.initial));
-    expect(() => multi.add(WorkpiecePlacement(w, Placement.initial)), throwsStateError);
-    multi.add(WorkpiecePlacement(w, Placement.right));
+  group('layout', () {
+    test('add', () {
+      var w = WorkSimulator(testCode);
+      w.simulate();
+      expect(w.physicalBox, equals(Rect(Point.zero(), Point(10, 10))));
+      var multi = MultiWorkpiece();
+      expect(() => multi.add(WorkpiecePlacement(w, Placement.up)), throwsStateError);
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      expect(() => multi.add(WorkpiecePlacement(w, Placement.initial)), throwsStateError);
+      multi.add(WorkpiecePlacement(w, Placement.right));
+    });
+
+    test('layout right', () {
+      var w = WorkSimulator(testCode);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(10, 10))));
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.safetyMargin = 0;
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(20, 10))));
+      multi.safetyMargin = 20;
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 10))));
+    });
+
+    test('layout right and up', () {
+      var w = WorkSimulator(testCode);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.add(WorkpiecePlacement(w, Placement.up));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(30, 30), Point(40, 40))));
+    });
+
+    test('layout right and upAlignLeft', () {
+      var w = WorkSimulator(testCode);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(0, 30), Point(10, 40))));
+      // add another one to the right
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(30, 30), Point(40, 40))));
+    });
+
+    test('layout right and upAlignLeft with tall items', () {
+      var w = WorkSimulator(testCode);
+      var wTall = WorkSimulator(testCodeTall);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(wTall, Placement.initial));
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 50))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(0, 40), Point(10, 50))));
+    });
+
+    test('layout right and upAlignLeft with multiple tall items', () {
+      var w = WorkSimulator(testCode);
+      var wTall = WorkSimulator(testCodeTall);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      multi.add(WorkpiecePlacement(wTall, Placement.right));
+      multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point(0, -5), Point(40, 40))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(0, 30), Point(10, 40))));
+      // add to right should 'skip' the tall object placed second
+      multi.add(WorkpiecePlacement(w, Placement.right));
+      multi.layout();
+      expect(multi.encompassingBox(), equals(Rect(Point(0, -5), Point(70, 40))));
+      expect(multi.machineBoxes.last, equals(Rect(Point(60, 30), Point(70, 40))));
+    });
   });
 
-  test('layout right', () {
-    var w = WorkSimulator(testCode);
-    var multi = MultiWorkpiece();
-    multi.add(WorkpiecePlacement(w, Placement.initial));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(10, 10))));
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.safetyMargin = 0;
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(20, 10))));
-    multi.safetyMargin = 20;
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 10))));
+  group('generate', () {
+
+    test('single', () {
+      var w = WorkSimulator(testCode);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial));
+      expect(() => multi.generateCode(), throwsStateError); // no layout yet
+      multi.layout();
+      expect(multi.generateCode(), equals(testCode));
+    });
+
+    test('multi', () {
+      var w = WorkSimulator(testCode);
+      var multi = MultiWorkpiece();
+      multi.add(WorkpiecePlacement(w, Placement.initial, description: 'first workpiece'));
+      multi.add(WorkpiecePlacement(w, Placement.right, description: 'second workpiece'));
+      multi.layout();
+      expect(multi.generateCode().contains('G10 L20 P1 X-20.0 Y10.0'), isTrue);
+      expect(multi.generateCode().contains('(Move origin for second workpiece)'), isTrue);
+      multi.add(WorkpiecePlacement(w, Placement.upAlignLeft, description: 'third workpiece'));
+      multi.layout();
+      expect(multi.generateCode().contains('G10 L20 P1 X40.0 Y-20.0'), isTrue);
+    });
+
   });
 
-  test('layout right and up', () {
-    var w = WorkSimulator(testCode);
-    var multi = MultiWorkpiece();
-    multi.add(WorkpiecePlacement(w, Placement.initial));
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.add(WorkpiecePlacement(w, Placement.up));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(30, 30), Point(40, 40))));
-  });
-
-  test('layout right and upAlignLeft', () {
-    var w = WorkSimulator(testCode);
-    var multi = MultiWorkpiece();
-    multi.add(WorkpiecePlacement(w, Placement.initial));
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(0, 30), Point(10, 40))));
-    // add another one to the right
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 40))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(30, 30), Point(40, 40))));
-  });
-
-  test('layout right and upAlignLeft with tall items', () {
-    var w = WorkSimulator(testCode);
-    var wTall = WorkSimulator(testCodeTall);
-    var multi = MultiWorkpiece();
-    multi.add(WorkpiecePlacement(wTall, Placement.initial));
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point.zero(), Point(40, 50))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(0, 40), Point(10, 50))));
-  });
-
-  test('layout right and upAlignLeft with multiple tall items', () {
-    var w = WorkSimulator(testCode);
-    var wTall = WorkSimulator(testCodeTall);
-    var multi = MultiWorkpiece();
-    multi.add(WorkpiecePlacement(w, Placement.initial));
-    multi.add(WorkpiecePlacement(wTall, Placement.right));
-    multi.add(WorkpiecePlacement(w, Placement.upAlignLeft));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point(0, -5), Point(40, 40))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(0, 30), Point(10, 40))));
-    // add to right should 'skip' the tall object placed second
-    multi.add(WorkpiecePlacement(w, Placement.right));
-    multi.layout();
-    expect(multi.encompassingBox(), equals(Rect(Point(0, -5), Point(70, 40))));
-    expect(multi.machineBoxes.last, equals(Rect(Point(60, 30), Point(70, 40))));
-  });
 }
